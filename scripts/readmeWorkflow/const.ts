4d59c26c4616e3c75615f1f32f2721548f066642
Loi Ng <catslinc0@gmail.com>
2024-09-20 19:25:20 +0800

    bookmark chore(release): LoiChat

--- scripts/readmeWorkflow/const.ts
+++ scripts/readmeWorkflow/const.ts
@@ -8,8 +8,8 @@ export interface DataItem {
 
 export const MARKET_URL = 'https://chat.theforage.cn/market';
 export const PLGUIN_URL = 'https://chat.theforage.cn/settings/agent';
-export const AGENT_EN_URL = 'https://chat-agents.theforage.cn/index.json';
-export const AGENT_CN_URL = 'https://chat-agents.theforage.cn/index.zh-CN.json';
+export const AGENT_EN_URL = 'https://chat-agents.lobehub.com/index.json';
+export const AGENT_CN_URL = 'https://chat-agents.lobehub.com/index.zh-CN.json';
 export const AGENT_REPO = 'https://github.com/lobehub/lobe-chat-agents';
 export const PLUGIN_EN_URL = 'https://chat-plugins.theforage.cn/index.json';
 export const PLUGIN_CN_URL = 'https://chat-plugins.theforage.cn/index.zh-CN.json';
